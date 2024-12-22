4839a3e87e71ca672cf44f4052c5d67470822a84
Raynesz <31424806+Raynesz@users.noreply.github.com>
2024-12-22 23:23:41 +0200

    Add GitHub Actions for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter({
+      // Options for static adapter
+      pages: "build", // Directory for built files
+      assets: "build", // Directory for assets
+      fallback: "index.html", // Set to 'index.html' for SPA fallback
+      precompress: false, // Enable gzip/brotli compression if needed
+    }),
+  },
 };
 
 export default config;
